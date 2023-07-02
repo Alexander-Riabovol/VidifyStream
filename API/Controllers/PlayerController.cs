03bcdc592e1b709ba639e7534e854930c26c9281
Alexander-Riabovol <alexander-riabovol@proton.me>
2023-07-02 20:00:53 +0200

    Make player pleasant to look at

--- API/Controllers/PlayerController.cs
+++ API/Controllers/PlayerController.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Logic.Services.VideoService;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     public class PlayerController : Controller
     {
-        [Route("/player/{fileName}")]
+        private readonly IVideoService _videoService;
+
+        public PlayerController(IVideoService videoService) 
+        {
+            _videoService = videoService;
+        }
+
+        [Route("/player/{videoId}")]
         // This will hide the method from swagger, preventing it from crushing.
         [ApiExplorerSettings(IgnoreApi = true)]
-        public IActionResult Index(string fileName)
+        public async Task<IActionResult> PlayVideoById(int videoId)
         {
-            var domainName = HttpContext.Request.Host;
-            this.ViewBag.FileRoute = $"//{domainName}/api/download/{fileName}";
+            var serviceResponse = await _videoService.GetVideo(videoId);
+            this.ViewBag.FileRoute = serviceResponse.Content?.SourceUrl;
             return View("~/Pages/Player.cshtml");
         }
     }
